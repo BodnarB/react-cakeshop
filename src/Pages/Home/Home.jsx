cacdd76a09be4f0c33dac61947028f5435df760b
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-01-24 12:59:39 +0100

    home css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,25 +10,35 @@ export default function Home({ addFunc }) {
     <>
       <Header />
       <main className='home'>
+        <div className='intro-img-fade'>
+          <img className='intro-cover-img' src="https://images.pexels.com/photos/461431/pexels-photo-461431.jpeg" alt="" />
+          <p className='intro-cover-text'>The best cakes in town</p>
+        </div>
         <section className='about-us'>
-          <div>
-            <h2>About us</h2>
-            <p className='home-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consectetur voluptatibus vitae quam quaerat porro, aperiam maiores temporibus illum reprehenderit qui quas ipsa ad quis?
-              Perspiciatis.</p>
-            <p className='home-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aspernatur, minus nemo atque porro nesciunt
-              nobis quidem id necessitatibus,
-              cupiditate aliquid magni a architecto repellendus ullam! Numquam, distinctio ducimus? Ut!</p>
+          <div className='max-width-about'>
+            <div>
+              <h2>About us</h2>
+              <p className='home-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Consectetur voluptatibus vitae quam quaerat porro, aperiam maiores temporibus illum reprehenderit qui quas ipsa ad quis?
+                Perspiciatis.</p>
+              <p className='home-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aspernatur, minus nemo atque porro nesciunt
+                nobis quidem id necessitatibus,
+                cupiditate aliquid magni a architecto repellendus ullam! Numquam, distinctio.</p>
+            </div>
+            <img className='intro-img' src={introImg} alt="" />
           </div>
-          <img className='intro-img' src={introImg} alt="" />
         </section>
         <section className='history'>
-          <h2>Our history</h2>
-          <p className='home-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eligendi eius optio porro aspernatur,
-            molestias fugit sint. Iusto atque et iure illo autem repudiandae quas quaerat, eligendi possimus velit
-            porro laborum odit, esse voluptatum est laboriosam! Architecto culpa excepturi, consequatur quam
-            labore eius, explicabo est alias tempora beatae quo quaerat!</p>
-          <img className='history-img' src="https://images.pexels.com/photos/6210908/pexels-photo-6210908.jpeg" alt="" />
+          <div className="max-width-history">
+            <div>
+              <h2>Our history</h2>
+              <p className='home-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eligendi eius optio porro aspernatur,
+                molestias fugit sint. Iusto atque et iure illo autem repudiandae quas quaerat, eligendi possimus velit
+                porro laborum odit, esse voluptatum est laboriosam! Architecto culpa excepturi, consequatur quam
+                labore eius, explicabo est alias tempora beatae quo quaerat!</p>
+            </div>
+            <img className='history-img' src="https://images.pexels.com/photos/6210908/pexels-photo-6210908.jpeg" alt="" />
+          </div>
         </section>
       </main>
     </>
