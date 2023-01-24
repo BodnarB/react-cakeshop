4eb208c796ee694de70d0ea9a840187c96b29df3
Boglarka Bodnar <bodnarbog94@gmail.com>
2023-01-24 14:39:29 +0100

    update prods css

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -11,18 +11,16 @@ export default function Products({ addFunc }) {
                 <div className='prod-card' key={uuidv4()}>
                     <img className='prod-img' src={prod.imgSrc} alt="" />
                     <div className='prod-card-container'>
-                        <p>{prod.prodTitle}</p>
-                        <p>{prod.prodPrice} $</p>
-                        <p>{prod.added}</p>
-                        <button
-                            className='prod-add-btn'
-                            disabled={prod.added}
+                        <div className='prod-info'>
+                            <p>{prod.prodTitle}</p>
+                            <p>{prod.prodPrice} $</p>
+                        </div>
+                        <button className='prod-add-btn' disabled={prod.prodQty > 0}
                             onClick={() => {
-                                prod.added = true;
                                 prod.prodQty += 1
                                 addFunc(prod)
                             }}>
-                            {prod.added === true ? <>Added to cart</> : <>Add to cart</>}
+                            {prod.prodQty > 0 ? <>Added to cart</> : <>Add to cart</>}
                         </button>
                     </div>
                 </div>)}
